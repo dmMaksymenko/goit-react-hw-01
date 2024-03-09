b873194809267c06cc5841ea045208c2af2bda51
dmMaksymenko <dmitriy.maksimenko928@gmail.com>
2024-03-09 18:57:30 +0000

    update

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,16 +1,16 @@
-import "./Profile.module.css";
+import css from "./Profile.module.css";
 function Profile({ name, tag, location, image, stats }) {
   return (
-    <div>
-      <div>
-        <img className="avatar" src={image} alt="User avatar" />
+    <div className={css.profileBox}>
+      <div className={css.avatarBox}>
+        <img className={css.avatar} src={image} alt="User avatar" />
         <p>{name}</p>
         <p>@{tag}</p>
         <p>{location}</p>
       </div>
-      <ul>
+      <ul className={css.statsList}>
         {Object.entries(stats).map(([label, value]) => (
-          <li key={label}>
+          <li className={css.statsListItem} key={label}>
             <span>{label}</span>
             <span> {value}</span>
           </li>
