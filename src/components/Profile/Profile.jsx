964c470001460cf7080d2521a90acebb29b9be6f
dmMaksymenko <dmitriy.maksimenko928@gmail.com>
2024-03-11 10:18:56 +0000

    styles update

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,15 +4,15 @@ function Profile({ name, tag, location, image, stats }) {
     <div className={css.profileBox}>
       <div className={css.avatarBox}>
         <img className={css.avatar} src={image} alt="User avatar" />
-        <p>{name}</p>
-        <p>@{tag}</p>
-        <p>{location}</p>
+        <p className={css.bold}>{name}</p>
+        <p className={css.greyText}>@{tag}</p>
+        <p className={css.greyText}>{location}</p>
       </div>
       <ul className={css.statsList}>
         {Object.entries(stats).map(([label, value]) => (
           <li className={css.statsListItem} key={label}>
             <span>{label}</span>
-            <span> {value}</span>
+            <span className={css.bold}> {value}</span>
           </li>
         ))}
       </ul>
